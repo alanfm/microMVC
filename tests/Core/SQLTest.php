188e3ac6767725b0eb8e31e34bdf2241c67c45ab
Alan Freire <alan_freire@msn.com>
2016-09-29 23:13:15 -0300

    Comentarios do código do arquivo SQLTest.php

--- tests/Core/SQLTest.php
+++ tests/Core/SQLTest.php
@@ -20,16 +20,25 @@ class SQLTest extends \PHPUnit_Framework_TestCase
      */
     protected $sql;
 
+    /**
+     * Executa as configurações iniciais
+     */
     public function setUp()
     {
         $this->sql = new SQL();
     }
 
+    /**
+     * Testa se foi criado a instancia da classe SQL
+     */
     public function testInstanceSQL()
     {
         $this->assertInstanceOf(SQL::class, $this->sql);
     }
 
+    /**
+     * Testa o método insert
+     */
     public function testInsert()
     {
         $this->sql->insert('tabela', ['col1'=>'val1', 'col2'=>'val2', 'col3'=>'val3']);
@@ -37,6 +46,9 @@ class SQLTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('INSERT INTO tabela (col1, col2, col3) VALUES (?, ?, ?)', $this->sql->get());
     }
 
+    /**
+     * Testa o método delete sem a clausula where
+     */
     public function testDelete()
     {
         $this->sql->delete('tabela');
@@ -44,6 +56,9 @@ class SQLTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('DELETE FROM tabela', $this->sql->get());
     }
 
+    /**
+     * Testa o método delete com a clausula where
+     */
     public function testDeleteById()
     {
         $this->sql->delete('tabela')->where(['id'=>1]);
@@ -51,6 +66,9 @@ class SQLTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('DELETE FROM tabela WHERE (id = ?)', $this->sql->get());
     }
 
+    /**
+     * Testa o método update sem a cláusula where
+     */
     public function testUpdate()
     {
         $this->sql->update('tabela', ['col1'=>'val1', 'col2'=>'val2']);
@@ -58,6 +76,9 @@ class SQLTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('UPDATE tabela SET col1 = ?, col2 = ?', $this->sql->get());
     }
 
+    /**
+     * Testa o método update com a cláusula where
+     */
     public function testUpdateById()
     {
         $this->sql->update('tabela', ['col1'=>'val1', 'col2'=>'val2'])->where(['id'=>2]);
@@ -65,6 +86,10 @@ class SQLTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('UPDATE tabela SET col1 = ?, col2 = ? WHERE (id = ?)', $this->sql->get());
     }
 
+    /**
+     * Testa do método select sem a cláusula where
+     * e com os campos passados por parametro
+     */
     public function testSelect()
     {
         $this->sql->select('tabela', ['col1', 'col2', 'col3']);
@@ -72,6 +97,10 @@ class SQLTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('SELECT col1, col2, col3 FROM tabela', $this->sql->get());
     }
 
+    /**
+     * Teste do método selec sem a clausula where
+     * e sem passar o parametro campos
+     */
     public function testSelectEmptyFields()
     {
         $this->sql->select('tabela');
